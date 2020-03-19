380ef1c2dff289673c474f3fc5b6c99bfe75f531
Cole <cole-s@users.noreply.github.com>
2020-03-19 17:15:01 -0400

    Added some base code to all of teh constrain classes to help start things off

--- src/Constraints.java
+++ src/Constraints.java
@@ -4,17 +4,64 @@ Class: Constraints
 Controls all of the different types of constrants needed for the AI to function
 */
 
-class Constraints{
+class Constraints {
+    private final int MAX_SIZE = 9;
     private HorizontalConstraint[] horizontal;
     private VerticalConstraint[] vertical;
     private BoxConstraint[] box;
 
-    public Constraints(){
+    public Constraints() {
+        constructHorizontal();
+        constructVertical();
+        constructBox();
+    } // end of constructor
 
-    }
+    // help constructor methods:
+
+    private void constructHorizontal() {
+        this.horizontal = new HorizontalConstraint[MAX_SIZE];
+
+        for(int index = 0; index < MAX_SIZE; index++){
+            this.horizontal[index] = new HorizontalConstraint();
+        } // end of for loop
+    } // end of constructHorizontal
+
+    private void constructVertical() {
+        this.vertical = new VerticalConstraint[MAX_SIZE];
+
+        for(int index = 0; index < MAX_SIZE; index++){
+            this.vertical[index] = new VerticalConstraint();
+        } // end of for loop
+    } // end of constructVertical
+
+    private void constructBox() {
+        this.box = new BoxConstraint[MAX_SIZE];
+
+        for(int index = 0; index < MAX_SIZE; index++){
+            this.box[index] = new BoxConstraint();
+        } // end of for loop
+    } // end of constructBox
+
+    // end of help constructor methods
 
     public HorizontalConstraint[] getHorizontal() { return this.horizontal; }
     public VerticalConstraint[] getVertical() { return this.vertical; }
     public BoxConstraint[] getBox() { return this.box; }
-    public void setHorizontal(HorizontalConstraint[] horizontal) { this.horizontal = horizontal; }
-}
+    public void setHorizontal(HorizontalConstraint[] horizontal) {
+        for(int index = 0; index < MAX_SIZE; index++){
+            this.horizontal[index] = new HorizontalConstraint(horizontal[index].getAvailable());
+        } // end of for loop
+    } // end of setHorizontal
+
+    public void setVertical(VerticalConstraint[] vertical) {
+        for(int index = 0; index < MAX_SIZE; index++){
+            this.vertical[index] = new VerticalConstraint(vertical[index].getAvailable());
+        } // end of for loop
+    } // end of setVertical
+
+    public void setBox(BoxConstraint[] box) {
+        for(int index = 0; index < MAX_SIZE; index++) {
+            this.box[index] = new BoxConstraint(box[index].getAvailable());
+        } // end of for loop
+    } // end of setBox
+} // end of Constraints Class
--- src/HorizontalConstraint.java
+++ src/HorizontalConstraint.java
@@ -1,3 +1,38 @@
-class HorizontalConstraint{
+class HorizontalConstraint {
+    private final int MAX_SIZE = 9;
     private int[] available;
-}
+
+    public HorizontalConstraint() {
+        this.available = new int[MAX_SIZE];
+        for(int index = 0; index < MAX_SIZE; index++){
+            this.available[index] = index+1;
+        } // end of for loop
+    } // end of constructor
+
+    public HorizontalConstraint(int[] available) {
+        setAvailable(available);
+    } // end of constructor
+
+    // getters and setters for the private variables
+    public int[] getAvailable() { return this.available; } // end of getAvailable
+    public void setAvailable(int[] available) {
+        for(int index = 0; index < MAX_SIZE; index++){
+            this.available[index] = available[index];
+        } // end of for loop
+    } // end of setAvailable
+
+    public int countAvailable() {
+        int count = 0;
+        for(int index = 0; index < MAX_SIZE; index++) {
+            if(this.available[index] != -1)
+                count++;
+            // end of if statement
+        } // end of for loop
+
+        return count;
+    } // end of countAvailable
+
+    public void removeAvailable(int index) {
+        this.available[index] = -1;
+    } // end of removeAvailable
+} // end of HorizontalConstrant Class
