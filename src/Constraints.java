d20e8635e35cdde3298b5d0ee1021b3117c01b56
cole-s <scha7843@kettering.edu>
2020-03-22 13:10:02 -0400

    Added some base functionality and fixed a future bug

--- src/Constraints.java
+++ src/Constraints.java
@@ -6,9 +6,9 @@ Controls all of the different types of constrants needed for the AI to function
 
 class Constraints {
     private final int MAX_SIZE = 9;
-    private HorizontalConstraint[] horizontal;
-    private VerticalConstraint[] vertical;
-    private BoxConstraint[] box;
+    private HorizontalConstraint[][] horizontal;
+    private VerticalConstraint[][] vertical;
+    private BoxConstraint[][] box;
 
     public Constraints() {
         constructHorizontal();
@@ -19,49 +19,77 @@ class Constraints {
     // help constructor methods:
 
     private void constructHorizontal() {
-        this.horizontal = new HorizontalConstraint[MAX_SIZE];
+        this.horizontal = new HorizontalConstraint[MAX_SIZE][MAX_SIZE];
 
-        for(int index = 0; index < MAX_SIZE; index++){
-            this.horizontal[index] = new HorizontalConstraint();
+        for(int rowindex = 0; rowindex < MAX_SIZE; rowindex++){
+            for(int colindex = 0; colindex < MAX_SIZE; colindex++) {
+                this.horizontal[rowindex][colindex] = new HorizontalConstraint();
+            } // end of for loop
         } // end of for loop
     } // end of constructHorizontal
 
     private void constructVertical() {
-        this.vertical = new VerticalConstraint[MAX_SIZE];
+        this.vertical = new VerticalConstraint[MAX_SIZE][MAX_SIZE];
 
-        for(int index = 0; index < MAX_SIZE; index++){
-            this.vertical[index] = new VerticalConstraint();
+        for(int rowindex = 0; rowindex < MAX_SIZE; rowindex++){
+            for(int colindex = 0; colindex < MAX_SIZE; colindex++) {
+                this.vertical[rowindex][colindex] = new VerticalConstraint();
+            } // end of for loop
         } // end of for loop
     } // end of constructVertical
 
     private void constructBox() {
-        this.box = new BoxConstraint[MAX_SIZE];
+        this.box = new BoxConstraint[MAX_SIZE][MAX_SIZE];
 
-        for(int index = 0; index < MAX_SIZE; index++){
-            this.box[index] = new BoxConstraint();
+        for(int rowindex = 0; rowindex < MAX_SIZE; rowindex++){
+            for(int colindex = 0; colindex < MAX_SIZE; colindex++) {
+                this.box[rowindex][colindex] = new BoxConstraint();
+            } // end of for loop
         } // end of for loop
     } // end of constructBox
 
     // end of help constructor methods
 
-    public HorizontalConstraint[] getHorizontal() { return this.horizontal; }
-    public VerticalConstraint[] getVertical() { return this.vertical; }
-    public BoxConstraint[] getBox() { return this.box; }
-    public void setHorizontal(HorizontalConstraint[] horizontal) {
-        for(int index = 0; index < MAX_SIZE; index++){
-            this.horizontal[index] = new HorizontalConstraint(horizontal[index].getAvailable());
+    public HorizontalConstraint[][] getHorizontal() { return this.horizontal; }
+    public VerticalConstraint[][] getVertical() { return this.vertical; }
+    public BoxConstraint[][] getBox() { return this.box; }
+    public void setHorizontal(HorizontalConstraint[][] horizontal) {
+        for(int rowindex = 0; rowindex < MAX_SIZE; rowindex++){
+            for(int colindex = 0; colindex < MAX_SIZE; colindex++) {
+                this.horizontal[rowindex][colindex] = new HorizontalConstraint(horizontal[rowindex][colindex].getAvailable());
+            } // end of for loop
         } // end of for loop
     } // end of setHorizontal
 
-    public void setVertical(VerticalConstraint[] vertical) {
-        for(int index = 0; index < MAX_SIZE; index++){
-            this.vertical[index] = new VerticalConstraint(vertical[index].getAvailable());
+    public void setVertical(VerticalConstraint[][] vertical) {
+        for(int rowindex = 0; rowindex < MAX_SIZE; rowindex++) {
+            for(int colindex = 0; colindex < MAX_SIZE; colindex++) {
+                this.vertical[rowindex][colindex] = new VerticalConstraint(vertical[rowindex][colindex].getAvailable());
+            } // end of for loop
         } // end of for loop
     } // end of setVertical
 
-    public void setBox(BoxConstraint[] box) {
-        for(int index = 0; index < MAX_SIZE; index++) {
-            this.box[index] = new BoxConstraint(box[index].getAvailable());
+    public void setBox(BoxConstraint[][] box) {
+        for(int rowindex = 0; rowindex < MAX_SIZE; rowindex++) {
+            for(int colindex = 0; colindex < MAX_SIZE; colindex++) {
+                this.box[rowindex][colindex] = new BoxConstraint(box[rowindex][colindex].getAvailable());
+            } // end of for loop
         } // end of for loop
     } // end of setBox
+
+    public int countAvailableMoves(int rowindex, int colindex) {
+        int count = 0;
+
+        count += this.horizontal[rowindex][colindex].countAvailable();
+        count += this.vertical[rowindex][colindex].countAvailable();
+        count += this.box[rowindex][colindex].countAvailable();
+
+        return count;
+    } // end of countAvailableMoves
+
+    public void removeMove(int rowindex, int colindex, int number) {
+        this.horizontal[rowindex][colindex].removeAvailable(number);
+        this.vertical[rowindex][colindex].removeAvailable(number);
+        this.box[rowindex][colindex].removeAvailable(number);
+    } // end of removeMove
 } // end of Constraints Class
--- src/Control.java
+++ src/Control.java
@@ -1,3 +1,6 @@
+import java.util.Hashtable;
+import java.io.*;
+
 public class Control {
 
-}
+} // end of Control Class
